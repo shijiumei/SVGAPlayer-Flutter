6818d684d4b44302b30226088a96e8d391d77774
zhaozhenhui <zhenhui2004@aliyun.com>
2025-07-06 18:57:13 +0800

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -155,7 +155,7 @@ class _SVGASampleScreenState extends State<SVGASampleScreen>
           Container(
               padding: const EdgeInsets.all(8.0),
               child: Text("Url: ${widget.image}",
-                  style: Theme.of(context).textTheme.subtitle2)),
+                  style: Theme.of(context).textTheme.titleLarge)),
           if (isLoading) LinearProgressIndicator(),
           Center(
             child: ColoredBox(
